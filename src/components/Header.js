0afe5f2823e8d38fdcf2b72a202d43acc74ad2f2
Aydoğan <aydogan.gokberk@gmail.com>
2021-03-27 15:43:27 +0300

    loading error css fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,16 @@ import Loading from "./Loading";
 const Header = (props) => {
 	if (props.error)
 		return (
-			<div className="flex flex-col items-center justify-center p-20">
-				<h1 className="p-3 text-5xl">😟</h1>
-				{props.city} not found.
-			</div>
+			<header className="font-thin text-center p-11">
+				<h1 className="p-5 text-8xl">😟</h1>
+				<span className="">{props.city} not found.</span>
+			</header>
 		);
 	if (!props.data) return <Loading />;
 	return (
 		<header className="p-12 font-thin text-center">
 			<h1 className="text-9xl">{props.data.current.temp_c}&#176;</h1>
-			<span className="capitalize ">{props.city}</span>
+			<span className="capitalize">{props.city}</span>
 		</header>
 	);
 };
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 const Loading = () => {
 	return (
-		<div className="flex items-center justify-center p-20 align-middle">
-			<span className="text-9xl justify-self-center animate-spin">⏳</span>
+		<div className="flex items-center justify-center align-middle p-14">
+			<span className="p-1 text-9xl justify-self-center animate-spin">⏳</span>
 		</div>
 	);
 };
