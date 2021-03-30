f15db0e74922b94d3f09a11e018eb62e57a43454
Aydoğan <aydogan.gokberk@gmail.com>
2021-03-31 00:35:32 +0300

    typo fix

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,6 @@
 const Loading = () => {
 	return (
-		<div className="flex flex-col items-center justify-center p-10 font-thin text-center text-white bg-blue-400 h-96sd">
+		<div className="flex flex-col items-center justify-center p-10 font-thin text-center text-white bg-blue-400 h-96">
 			<span className="p-1 text-9xl justify-self-center animate-spin">⏳</span>
 		</div>
 	);
