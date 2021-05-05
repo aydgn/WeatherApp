291c204d4d73532f23d91be59d1893c72c0aba3e
Aydoğan <aydogan.gokberk@gmail.com>
2021-05-05 14:05:13 +0300

    show hide toggle

--- src/components/GetLocationButton.js
+++ src/components/GetLocationButton.js
@@ -1,37 +1,34 @@
-import { useState } from "react";
-
 const GetLocationButton = ({ setCity }) => {
-	const [getLocation, setgetLocation] = useState(false);
-	const getCurrentLocation = (lat, long) => {
-		if ("geolocation" in navigator) {
-			/* Eğer cihazda GPS özellikleri aktifse tahmini kordinantlarını alıyoruz. */
-			navigator.geolocation.getCurrentPosition(function (position) {
-				lat = position.coords.latitude;
-				long = position.coords.longitude;
+  const getCurrentLocation = (lat, long) => {
+    if ("geolocation" in navigator) {
+      /* Eğer cihazda GPS özellikleri aktifse tahmini kordinantlarını alıyoruz. */
+      navigator.geolocation.getCurrentPosition(function (position) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
 
-				fetch(
-					`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${lat},${long}`
-				)
-					.then((response) => response.json())
-					.then((city) => setCity(city.location.region));
-			});
-		} else {
-			console.log("Not Available");
-			alert("We your location data in order to use this feature.");
-		}
-	};
+        fetch(
+          `https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${lat},${long}`
+        )
+          .then((response) => response.json())
+          .then((city) => setCity(city.location.region));
+      });
+    } else {
+      console.log("Not Available");
+      alert("We your location data in order to use this feature.");
+    }
+  };
 
-	return (
-		<div className="flex flex-col items-center justify-center flex-1">
-			<span>or</span>
-			<button
-				className="w-full py-3 border shadow"
-				onClick={getCurrentLocation}
-			>
-				📍 Get my location
-			</button>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center flex-1">
+      <span>or</span>
+      <button
+        className="w-full py-3 border shadow"
+        onClick={getCurrentLocation}
+      >
+        📍 Get my location
+      </button>
+    </div>
+  );
 };
 
 export default GetLocationButton;
